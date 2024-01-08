62166c43861542f39a4f7862be194e92eb9e268b
myjhye <t01087633799@gmail.com>
2024-01-08 22:54:29 +0900

    products service, serviceImpl 생성 - get (단일)

--- src/main/java/com/shopping/shopping/service/ProductsService.java
+++ src/main/java/com/shopping/shopping/service/ProductsService.java
@@ -3,5 +3,9 @@ package com.shopping.shopping.service;
 import com.shopping.shopping.dto.ProductsDto;
 
 public interface ProductsService {
+    // 상품 등록
     ProductsDto createProducts(ProductsDto productsDto);
+
+    // 상품 단일 조회
+    ProductsDto getProductById(Long productId);
 }
--- src/main/java/com/shopping/shopping/service/impl/ProductsServiceImpl.java
+++ src/main/java/com/shopping/shopping/service/impl/ProductsServiceImpl.java
@@ -2,6 +2,7 @@ package com.shopping.shopping.service.impl;
 
 import com.shopping.shopping.dto.ProductsDto;
 import com.shopping.shopping.entity.Products;
+import com.shopping.shopping.exception.ResourceNotFoundException;
 import com.shopping.shopping.mapper.ProductsMapper;
 import com.shopping.shopping.repository.ProductsRepository;
 import com.shopping.shopping.service.ProductsService;
@@ -13,6 +14,8 @@ import org.springframework.stereotype.Service;
 public class ProductsServiceImpl implements ProductsService {
 
     private ProductsRepository productsRepository;
+
+    // 상품 등록
     @Override
     public ProductsDto createProducts(ProductsDto productsDto) {
 
@@ -20,5 +23,15 @@ public class ProductsServiceImpl implements ProductsService {
         Products savedProducts = productsRepository.save(products);
 
         return ProductsMapper.mapToProductsDto(savedProducts);
+    }
+
+    // 상품 단일 조회
+    @Override
+    public ProductsDto getProductById(Long productId) {
+
+        Products product = productsRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("상품이 존재하지 않음 : " + productId));
+
+        return ProductsMapper.mapToProductsDto(product);
     }
 }
