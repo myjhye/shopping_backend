ae15c60012d019cacde80f50dba146f6a4af9593
myjhye <t01087633799@gmail.com>
2024-01-08 21:12:38 +0900

    products 엔티티 생성

---
+++ src/main/java/com/shopping/shopping/entity/Products.java
@@ -0,0 +1,30 @@
+package com.shopping.shopping.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String category;
+    private String image;
+    private String title;
+    private String description;
+    private String price;
+    private String gender;
+    private String options;
+    private String date;
+
+}
