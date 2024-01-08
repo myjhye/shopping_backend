a0dce48eac6fb4d9f00a8ddf90e099bd1ae857da
myjhye <t01087633799@gmail.com>
2024-01-08 21:23:37 +0900

    products dto 생성

---
+++ src/main/java/com/shopping/shopping/dto/ProductsDto.java
@@ -0,0 +1,22 @@
+package com.shopping.shopping.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductsDto {
+    private long id;
+    private String category;
+    private String image;
+    private String title;
+    private String description;
+    private String price;
+    private String gender;
+    private String options;
+    private String date;
+}
