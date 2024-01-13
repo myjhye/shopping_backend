08ada0cd81c7c28af3e88d026987997b4574ae9e
myjhye <t01087633799@gmail.com>
2024-01-13 18:49:04 +0900

    database authentication : 데이터베이스의 사용자 정보로 사용자 인증

--- src/main/java/com/shopping/shopping/config/SpringSecurityConfig.java
+++ src/main/java/com/shopping/shopping/config/SpringSecurityConfig.java
@@ -1,22 +1,24 @@
 package com.shopping.shopping.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SpringSecurityConfig {
+
+    private UserDetailsService userDetailsService;
 
     // password 인코더
     @Bean
@@ -43,22 +45,10 @@ public class SpringSecurityConfig {
         return http.build();
     }
 
-    // 사용자 정보(jh, admin의 username, pw, role)을 메모리에 저장
     @Bean
-    public UserDetailsService userDetailsService() {
-
-        UserDetails jh = User.builder()
-                .username("jh")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("password"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(jh, admin);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
+
+
 }
