bed09c0000eceb8ed0a6aec610f5400ada9827f7
myjhye <t01087633799@gmail.com>
2024-01-13 17:09:43 +0900

    CustomerUserDetailService 클래스 생성 : 사용자 인증용

---
+++ src/main/java/com/shopping/shopping/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.shopping.shopping.security;
+
+import com.shopping.shopping.entity.User;
+import com.shopping.shopping.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저이름이나 이메일에 해당하는 유저가 없습니다"));
+
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                usernameOrEmail,
+                user.getPassword(),
+                authorities
+        );
+    }
+}
