44184fe905ac44ccfabdcc1160a323e8edec4cd4
myjhye <t01087633799@gmail.com>
2024-01-13 12:29:09 +0900

    User, Role 레포지토리 생성

---
+++ src/main/java/com/shopping/shopping/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.shopping.shopping.repository;
+
+import com.shopping.shopping.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
