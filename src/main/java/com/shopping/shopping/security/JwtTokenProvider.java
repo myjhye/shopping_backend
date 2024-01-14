a948e5470c9947f076320afbedaf34c7eaa91376
myjhye <t01087633799@gmail.com>
2024-01-15 00:20:52 +0900

    JwtTokenProvider 유틸리티 클래스 생성 : JWT 토큰 생성, 검증 도와주는 클래스

---
+++ src/main/java/com/shopping/shopping/security/JwtTokenProvider.java
@@ -0,0 +1,61 @@
+package com.shopping.shopping.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+    @Value("${app.jwt-expiration-milliseconds}")
+    private Long jwtExpirationDate;
+
+    // jwt 토큰 생성
+    public String generateToken(Authentication authentication) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expireDate = new Date(currentDate.getTime() + jwtExpirationDate);
+        String token = Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(expireDate)
+                .signWith(key())
+                .compact();
+
+        return token;
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    // jwt에서 username 조회
+    public String getUsername(String token) {
+
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    // jwt 토큰 유효성 검사
+    public boolean validateToken(String token) {
+
+        Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parse(token);
+
+        return true;
+    }
+}
